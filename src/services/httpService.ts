cbdbfffc4f3dde456c65db08017daf336fe5adf9
Alessandra Canuto <ale.canutto@gmail.com>
2022-07-08 16:12:58 -0300

    Update httpService.ts

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3001'
-    : 'https://glitch.com/~igti-react-backend-m3-acanuto';
+    : 'https://igti-react-backend-dp3-acanuto.glitch.me';
+	
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
